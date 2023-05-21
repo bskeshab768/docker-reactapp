deac4502e805c723b2c16639202807f8c2b8503b
bskeshab768 <133929326+bskeshab768@users.noreply.github.com>
2023-05-21 22:23:58 +0545

    Create MainContent.js

---
+++ src/MainContent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const MainContent = () => {
+  return (
+    <main>
+      <h2>Welcome to My Page!</h2>
+      <p>This is a simple static page.</p>
+      <p>Feel free to customize it according to your needs.</p>
+    </main>
+  );
+};
+
+export default MainContent;
